9cd4220877292e5597c938a30697195f0c7a9b84
yamini <bommiyamini.009@gmail.com>
2022-09-25 18:45:29 +0530

    changes commit

--- resources/views/admin/customer.blade.php
+++ resources/views/admin/customer.blade.php
@@ -5,20 +5,15 @@
 <p class="alert alert-info">{{ Session::get('message') }}</p>
 @endif
 
- <main class="py-4 bg-surface-secondary">
-
-     <div class="container-fluid">
-
-         <div class="row g-6 mb-4 ">
-             <div class="col-xl-12 col-sm-12 col-12">
-                 <div class="card shadow border-0">
-                     <div class="card-body -mt-5 -mb-4">
-                         <div class="row">
-                             <div class="col mt-10">
-
-                                 <div class="table-responsive">
-                                     <table class="table table-hover table-nowrap">
-                                         <thead class="thead-light">
+<main class="py-6 bg-surface-secondary">
+            <div class="container-fluid">
+                <div class="card shadow border-0 mb-7">
+                    <div class="card-header">
+                        <h5 class="mb-0">CUSTOMERS</h5>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-nowrap">
+                            <thead class="thead-light">
                                              <tr>
                                                  <th scope="col">S.No</th>
                                                  <th scope="col">Name</th>
@@ -28,7 +23,7 @@
                                                  <th scope="col">Update</th>
                                                  <th scope="col">Action</th>
 
-                                                 <th></th>
+                                           
 
 
                                              </tr>
@@ -48,20 +43,21 @@
                                                  <td>
                                                      {{ $list['mobile'] }}
                                                  </td>
-                                                 <td>
+                                                 <td class="text-primary">
 
                                                      {{ $list['created_at'] }} <span>
                                                  </td>
-                                                 <td>
+                                                 <td class="text-primary">
                                                      {{ $list['updated_at'] }}
                                                  </td>
 
                                                  <td>
                                                      <a href="{{url('admin/customer/delete/')}}/{{$list->id}}">
-                                                         <button type="button"
-                                                             class="btn btn-sm btn-square btn-neutral text-danger-hover">
-                                                             <i class="bi bi-trash"></i>
-                                                         </button></a>
+                                                     <div >
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-700 hover:text-red-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                              </svg>
+</div></a>
                                                  </td>
                                              </tr>
                                              <tr>
--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -10,18 +10,22 @@
 <br>
 @if(Session('role')=='Admin')
 <a href="product/add_product">
-    <button type="button" class="btn btn-success btn btn-primary btn-sm">
+    <button type="button" class="btn btn-success btn btn-primary btn-sm ">
         Add product 
     </button>
 </a>
 @endif
-<br>
-<div class="row m-t-30">
-    <div class="col-md-12">
-        <!-- DATA TABLE-->
-        <div class="table-responsive m-b-40">
-            <table class="table table-borderless table-data3">
-                <thead>
+
+<main class="py-6 bg-surface-secondary">
+            <div class="container-fluid">
+                <div class="card shadow border-0 mb-7">
+                    <div class="card-header">
+                        <h5 class="mb-0">PRODUCTS</h5>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-hover table-nowrap">
+                            <thead class="thead-light">
+                                <tr>
                      <tr>
                                     <th scope="col">S.No</th>
                                     <th scope="col">Product Name</th>
@@ -54,9 +58,9 @@
                         <td>{{$list->quantity}}</td>
                         <td>
                            @if($list['status']==1)
-                           <button type="button" class="btn btn-primary btn btn-primary btn-sm">In Stock</button>
+                          In Stock
                           @elseif($list['status']==0)
-                         <button type="button" class="btn btn-warning btn btn-primary btn-sm">Out of Stack</button>
+                         Out of Stack
                         @endif
                         </td>
                         @if(Session('role')=='Admin')
@@ -64,11 +68,16 @@
                         <div class="btn-group">
                              <form action="{{url('admin/product/')}}/{{$list->id}}" method="post">
                               @csrf @method('DELETE')
-                          <button type="submit" class="btn btn-danger btn btn-primary btn-sm mb-2 mr-2">Delete</button>
+                              <a href="#" class="text-sm text-blue-500  ">Edit</a>
                             </form>
                               <form action="{{url('admin/product/')}}/{{$list->id}}" method="post">
                               @csrf @method('GET')
-                            <button type="submit" class="btn btn-success btn btn-primary btn-sm mb-2 mr-2">Edit</button></form>
+                              <div class="pl-4">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-700 hover:text-red-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                              </svg>
+</div>
+                                              
                             </div>
                         </td>
                         @endif
@@ -80,5 +89,5 @@
         <!-- END DATA TABLE-->
     </div>
 </div>
-
+</main>
 @endsection
